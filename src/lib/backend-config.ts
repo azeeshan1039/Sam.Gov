a512eb6fee4f2e35abd55b4c1a40bcb2e12f3475
azeeshan1039 <azeeshan.business@gmail.com>
2026-03-05 22:01:18 +0500

    latest deployement

--- src/lib/backend-config.ts
+++ src/lib/backend-config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = process.env.BACKEND_URL || 'https://sam-gov-backend-1nvo.onrender.com';
+export const BACKEND_URL = process.env.BACKEND_URL || 'https://sam-gov-backend-1.onrender.com';
